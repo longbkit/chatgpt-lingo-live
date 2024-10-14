789793e9dda1eec3e91f497b7501ec46e38890a9
Long Luong <longbkit@gmail.com>
2024-10-14 10:52:38 +1000

    reduce padding for small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ChatMessages from "@/components/ChatMessages";
 
 export default function Home() {
   return (
-    <div className="p-2 sm:p-4 min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 flex items-center justify-center">
+    <div className="p-2 min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 flex items-center justify-center">
         <ChatMessages />
     </div>
   );
