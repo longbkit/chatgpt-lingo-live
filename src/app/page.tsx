bc76521853be2f47302c423ee47ea24a38269b36
Long Luong <longbkit@gmail.com>
2024-10-14 10:59:17 +1000

    adjust layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import ChatMessages from "@/components/ChatMessages";
 
 export default function Home() {
   return (
+    <>
+     <div className="hidden sm:block fixed inset-0 bg-gradient-to-br from-blue-100 to-purple-100 -z-10" />
     <ChatMessages />
+    </>
   );
 }
