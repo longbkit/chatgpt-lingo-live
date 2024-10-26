9bd860a9774ec2043c0d480974a6825447242702
Long Luong <longbkit@gmail.com>
2024-10-26 21:40:59 +1000

    fix build with generated dtos

--- src/apis/generated/nestjs-dto/create-learned-dictionary.dto.ts
+++ src/apis/generated/nestjs-dto/create-learned-dictionary.dto.ts
@@ -1,34 +1,5 @@
-import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsDateString,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-} from 'class-validator';
-
 export class CreateLearnedDictionaryDto {
-  @ApiProperty({
-    type: 'string',
-  })
-  @IsNotEmpty()
-  @IsString()
   status: string;
-  @ApiProperty({
-    type: 'string',
-    format: 'date-time',
-    required: false,
-    nullable: true,
-  })
-  @IsOptional()
-  @IsDateString()
   next_review?: Date | null;
-  @ApiProperty({
-    type: 'string',
-    format: 'date-time',
-    required: false,
-    nullable: true,
-  })
-  @IsOptional()
-  @IsDateString()
   last_reviewed?: Date | null;
 }
